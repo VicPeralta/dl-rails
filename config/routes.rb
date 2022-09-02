6bd75950956586fb6e599427edfd55fb22f6fc27
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-09-01 23:34:58 -0500

    Update routes
    modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "api#show"
-  get 'api/greeting/', to: 'api#show'
+  root "students#index"
+  get 'grades/student/:student_id', to: 'grades#by_student'
+  get 'grades/course/:course_id', to: 'grades#by_course'
+  resources :students, :courses, :grades
 end
